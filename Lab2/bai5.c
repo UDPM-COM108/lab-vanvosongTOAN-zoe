cf3961d7d555ad9bb081ee6d9970e6d6b503ba05
ThienToan <cmmsakura@gmail.com>
2025-11-11 12:57:06 +0700

    nop bài 5 Lab2

--- Lab2/bai5.c
+++ Lab2/bai5.c
@@ -7,17 +7,23 @@ int main(){
     printf("Đây là chương trình tính tính chu vi tam giác vuông của ToanThienPham(PS49327).\n");
 
     //Yêu cầu nhập độ dài cạnh
-    printf("Vui lòng nhập độ dài cạnh AB");
+    printf("Vui lòng nhập độ dài cạnh AB: ");
     scanf("%f",&ab);
-    printf("Vui lòng nhập độ dài cạnh BC");
+    printf("Vui lòng nhập độ dài cạnh BC: ");
     scanf("%f",&bc);
-    printf("Vui lòng nhập đồ dài cạnh CA");
+    printf("Vui lòng nhập đồ dài cạnh CA: ");
     scanf("%f",&ca);
 
     //tính chu vui tam giác vuông
     float chuvi = ab+bc+ca;
-
+    float dientich = ab*ca/2; 
+    
+    
+    
+    
+    
     //xuat ket qua
     printf("Chu vi tam giác là: %.1f\n",chuvi);
+    printf("Diện tích tam giác là: %.1f\n",dientich);
     return(0);
 }
