e817192cc8ffb57f9d532c43395907e28c4ce2d5
ThienToan <cmmsakura@gmail.com>
2025-11-11 12:53:02 +0700

    nop bai 5 Lab 2

---
+++ Lab2/bai5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    float ab,bc,ca;
+    //giới thiệu bài là
+    printf("Bài 5: Xây dựng chương trình tính chu vui tam giác vuông\n");
+    //giới thiệu người làm
+    printf("Đây là chương trình tính tính chu vi tam giác vuông của ToanThienPham(PS49327).\n");
+
+    //Yêu cầu nhập độ dài cạnh
+    printf("Vui lòng nhập độ dài cạnh AB");
+    scanf("%f",&ab);
+    printf("Vui lòng nhập độ dài cạnh BC");
+    scanf("%f",&bc);
+    printf("Vui lòng nhập đồ dài cạnh CA");
+    scanf("%f",&ca);
+
+    //tính chu vui tam giác vuông
+    float chuvi = ab+bc+ca;
+
+    //xuat ket qua
+    printf("Chu vi tam giác là: %.1f\n",chuvi);
+    return(0);
+}
